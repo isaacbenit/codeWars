b29526468a5843f01c8937516be5fe1b34dfeb92
isaacbenit <isaac.irakoze24snhu@keplercollege.ac.rw>
2025-07-22 14:52:56 +0200

    Add solution - CodeHub

---
+++ 3-kyu/how-many-are-smaller-than-me-ii/how-many-are-smaller-than-me-ii.js
@@ -0,0 +1,22 @@
+// function smaller(arr) {
+//     //code me
+//     let z=[]
+//     let k=0
+//         while(k<arr.length){
+//             z.push(arr.filter((n)=>n<arr[0]).length)
+//             k++
+//             arr=arr.slice(1)
+//         }
+//         z.push(0)
+//         return z
+//  }
+//  console.log(smaller([1, 1, -1, 0, 0]))
+​
+function smaller(nums) {
+let newArray =[]
+for(index in nums){
+    newArray.push(nums.slice(index).filter(a=>a< nums[index]).length)
+}
+return newArray
+}
+console.log(smaller([1, 2, 3]))
