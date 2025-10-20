c40eee4b0c2e7281ef51123b6d697b0141bf16c4
isaacbenit <isaac.irakoze24snhu@keplercollege.ac.rw>
2025-10-20 10:44:42 +0200

    Update solution - CodeHub

--- 8-kyu/array-plus-array/array-plus-array.java
+++ 8-kyu/array-plus-array/array-plus-array.java
@@ -1,3 +1,4 @@
+ 
 public class Sum {
   int sum1 = 0;
   int sum2 = 0;
@@ -5,10 +6,10 @@ public class Sum {
   public static int arrayPlusArray(int[] arr1, int[] arr2) {
     // arr1 + arr2 is not working...
     for(int i =0; i< arr1.length;i++){
-      sum1 += arr1[i];
+      sum1 = sum1 +arr1[i];
     }
     for(int a =0; a< arr2.length;a++){
-      sum2 += arr2[a];
+      sum2 = sum2 + arr2[a];
     }
     return sum1+sum2;
   }
