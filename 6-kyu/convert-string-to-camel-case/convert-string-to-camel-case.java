8f089283572708b474d7bd370f7d0eec2954be33
isaacbenit <isaac.irakoze24snhu@keplercollege.ac.rw>
2025-10-20 15:08:54 +0200

    Update solution - CodeHub

--- 6-kyu/convert-string-to-camel-case/convert-string-to-camel-case.java
+++ 6-kyu/convert-string-to-camel-case/convert-string-to-camel-case.java
@@ -1,14 +1,11 @@
-// import java.lang.StringBuilder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
 class Solution{
 ​
   static String toCamelCase(String s){
-//      s=s.trim();
-//     System.out.println(s);
     String[] sentenceTest = s.split("");
-        List<String> sentence1 = Arrays.asList(sentenceTest);
+//         List<String> sentence1 = Arrays.asList(sentenceTest);
     String[] word ={} ;
         
         if(sentence1.contains("-")){
@@ -18,8 +15,6 @@ class Solution{
         if(sentence1.contains("_")){
             word =  s.split("_");
         } 
-//      String[] word = s.split("-");
-        ArrayList<String[]> newSentence = new ArrayList<>();
         String newWord = "";
         for (int a=0;a<word.length;a++){
             String[] eles = word[a].split("");
@@ -31,8 +26,6 @@ class Solution{
             }
             String neweles = String.join("",eles);
             newWord = newWord + neweles;
-            // newSentence.add(eles);
-            // System.out.println(Arrays.toString(eles));
         }
         System.out.println((newWord));
         
